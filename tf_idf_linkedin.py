d8804e9f2917e2b0e8c2f04ae6c8877949ef9e63
Ekta Grover <ekta1007@gmail.com>
2013-08-23 13:48:23 +0530

    Update tf_idf_linkedin.py

--- tf_idf_linkedin.py
+++ tf_idf_linkedin.py
@@ -74,18 +74,16 @@ all_tips = []
 # add docs here
 for tip in doc_list:
     tokens = tokenizer.tokenize(tip)
- 
-    bi_tokens = bigrams(tokens)
+    
+	tokens = [token.lower() for token in tokens if len(token) > 2 and token not in stopwords]
+	
+	bi_tokens = bigrams(tokens)
     tri_tokens = trigrams(tokens)
-    tokens = [token.lower() for token in tokens if len(token) > 2]
-    tokens = [token for token in tokens if token not in stopwords]
- 
+
+    # doing some cosmetics for printing now...
     bi_tokens = [' '.join(token).lower() for token in bi_tokens]
-    bi_tokens = [token for token in bi_tokens if token not in stopwords]
- 
     tri_tokens = [' '.join(token).lower() for token in tri_tokens]
-    tri_tokens = [token for token in tri_tokens if token not in stopwords]
-     
+  
     final_tokens = []
     final_tokens.extend(tokens)
     final_tokens.extend(bi_tokens)
